d81ff95ac84d981bf83d431061a855f48632b1fb
Nathan Otto <nathan@Nathans-MacBook-Pro.local>
2016-02-26 14:02:54 -0800

    add first client test

---
+++ Objects/Client.cs
@@ -0,0 +1,91 @@
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System;
+
+namespace HairSalon
+{
+  public class Client
+  {
+    private int _id;
+    private string _name;
+    private int _stylistId;
+
+    public Client(string Name, int StylistId, int Id = 0)
+    {
+      _id = Id;
+      _name = Name;
+      _stylistId = StylistId;
+
+    }
+
+    public override bool Equals(System.Object otherClient)
+    {
+      if(!(otherClient is Client))
+      {
+        return false;
+      }
+      else
+      {
+        Client newClient = (Client) otherClient;
+        bool idEquality = this.GetId() == newClient.GetId();
+        bool nameEquality = this.GetName() == newClient.GetName();
+        return (idEquality && nameEquality);
+      }
+    }
+
+    public int GetId()
+    {
+      return _id;
+    }
+    public string GetName()
+    {
+      return _name;
+    }
+    public void SetName(string newName)
+    {
+      _name = newName;
+    }
+    public int GetStylistId()
+    {
+      return _stylistId;
+    }
+    public void SetStylistId(id newStylistId)
+    {
+      _stylistId = newStylistId;
+    }
+
+    public static List<Client> GetAll()
+    {
+      List<Client> allClients = new List<Client>{};
+
+      SqlConnection conn = DB.Connection();
+      SqlDataReader rdr = null;
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("SELECT * FROM clients;", conn);
+      rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        int clientId = rdr.GetInt32(0);
+        string clientName == rdr.GetString(1);
+        int clientStylistId == rdr.GetInt32(2);
+        Client newClient = new Client(clientName, clientStylistId, clientId);
+        allClients.Add(newClient);
+      }
+
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+
+      return allClients;
+    }
+
+
+  }
+}
